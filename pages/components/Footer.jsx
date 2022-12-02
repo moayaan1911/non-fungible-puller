36106caad5dc023e194cb00a6c71729fb95c387e
Mohammad Ayaan Siddiqui <ayaangames@gmail.com>
2022-12-02 07:45:55 +0530

    updated footer

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -26,20 +26,6 @@ const Footer = () => {
           <i className="fa-brands fa-ethereum"></i>
         </span>
       </div>
-      {/* <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" className="hover:underline">Contact</a>
-        </li>
-    </ul> */}
     </footer>
   );
 };
